64b19e8f4d0a7b429a426660ad576fff634149ad
jonadepr <jonadepr@gmail.com>
2020-07-16 12:24:25 +0200

    router 2

---
+++ src/views/UserList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="lista-usuarios">
+    <h1>Lista de usuarios</h1>
+    <b-table striped hover :items="items"></b-table>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            items: [
+          { first_name: 'Dickerson', last_name: 'Macdonald', score: 10000 },
+          { first_name: 'Larsen', last_name: 'Shaw', score: 9000 },
+          { first_name: 'Geneva', last_name: 'Wilson', score: 8000 },
+          { first_name: 'Jami', last_name: 'Carney', score: 7000 }
+        ]
+        }
+    },
+
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Usuario.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Detalle usuario</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

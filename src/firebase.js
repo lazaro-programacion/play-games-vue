3eb5bc69f8076ccb61b6ad9893226fb7cc80d4d3
Jose Lazaro <lazaro0089@gmail.com>
2020-07-16 09:57:40 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase, { firestore } from 'firebase/app'
+import firebase from 'firebase/auth'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBknOh2kqsOUrTFkkdhvkNwlWixzdi2h_k",
+  authDomain: "play-vue-bcfd0.firebaseapp.com",
+  databaseURL: "https://play-vue-bcfd0.firebaseio.com",
+  projectId: "play-vue-bcfd0",
+  storageBucket: "play-vue-bcfd0.appspot.com",
+  messagingSenderId: "678148662771",
+  appId: "1:678148662771:web:727b529bf3698787d89e86"
+  };
+
+  const db = firebase.firestore()
+  const auth = firebase.auth()
+
+  firebase.initializeApp(firebaseConfig);
+
+  export {db, auth}

18cfdf7f1b5259247a50a39d6836407e00322bdc
pacomonle <franciscomonleonleal@gmail.com>
2020-07-16 10:38:35 +0200

    config firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
-import firebase, { firestore } from 'firebase/app'
-import firebase from 'firebase/auth'
+import firebase from 'firebase/app'
+import 'firebase/auth'
 import 'firebase/firestore'
 
 const firebaseConfig = {

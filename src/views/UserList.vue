8a370a958d9f311fd993f1cdccd419690455b1dd
cizquierdof <cizquierdof@gmail.com>
2020-07-17 12:47:30 +0200

    lista usuarios

--- src/views/UserList.vue
+++ src/views/UserList.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="lista-usuarios">
     <h1>Lista de usuarios</h1>
-    <b-table striped hover :items="items"></b-table>
+    <ul>
+      <li v-for="user in usuarios" :key="user.id">
+        {{user.displayName}}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -9,15 +13,20 @@
 export default {
     data() {
         return {
-            items: [
-          { first_name: 'Dickerson', last_name: 'Macdonald', score: 10000 },
-          { first_name: 'Larsen', last_name: 'Shaw', score: 9000 },
-          { first_name: 'Geneva', last_name: 'Wilson', score: 8000 },
-          { first_name: 'Jami', last_name: 'Carney', score: 7000 }
-        ]
         }
     },
-
+    async created() {
+      try {
+        await this.$store.dispatch('getUsers');
+      } catch (error) {
+        console.error(error);
+      }    
+    },
+    computed: {
+      usuarios(){
+        return this.$store.state.usuarios
+      }
+    },
 };
 </script>
 

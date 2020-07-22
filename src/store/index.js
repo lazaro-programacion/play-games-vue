bdb6bad84165c503f8ea73691b4eebb276e2d541
cizquierdof <cizquierdof@gmail.com>
2020-07-22 19:40:07 +0200

    view usuario

--- src/store/index.js
+++ src/store/index.js
@@ -15,7 +15,8 @@ export default new Vuex.Store({
     },
     rankin: [],
     error: null,
-    usuarios:[]
+    usuarios:[],
+    usersPoints:[]
   },
 
   mutations: {
@@ -35,21 +36,30 @@ export default new Vuex.Store({
     },
     setUsers(state, usuarios){
       state.usuarios = usuarios;
+    },
+    setUsersPoints(state, usersPoints){
+      state.usersPoints=usersPoints;
     }
- 
 
+  },
+  getters:{
+    getUsers(state){
+      return state.usuarios;
+    }
   },
   actions: {
     getUsers({commit}){
-      db.collection('usuarios').get().then(
+      db.collection('users').get().then(
        res=> {
          const docsUsers=res.docs.map(
            item=>{
              const data=item.data();
              return{
-               id: item.id,
-               displayName: data.displayName,
-               rol: item.rol
+               email: item.id,
+               name: data.name,
+               about:data.about,
+               rol: data.rol,
+               avatar: data.avatar
              }
             }
          )
@@ -58,7 +68,24 @@ export default new Vuex.Store({
        }
       )
     },
-    
+    getPuntos({commit}){
+      db.collection('puntMemori').get().then(
+        res=>{
+          const docsPuntos=res.docs.map(
+            item=>{
+              const data=item.data();
+              return {
+                puntuacion: data.puntuacion,
+                email: item.id,
+                nombre:data.nombre
+              }
+            }
+          );
+          console.log('puntos', docsPuntos);
+          commit('setUsersPoints',docsPuntos);
+        }
+      )
+    },
     crearUsuario({ commit }, usuario) {
       auth
         .createUserWithEmailAndPassword(usuario.email, usuario.pass1)
--- src/views/UserList.vue
+++ src/views/UserList.vue
@@ -1,45 +1,90 @@
 <template>
-  <div class="lista-usuarios container">
-    <h1>Lista de usuarios</h1>
-    <table class="table table-hover table-dark">
-  <thead class="thead-light">
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Nombre:</th>
-      <th scope="col">Rol</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr v-for="(user, index) in usuarios" :key="index">
-      <th scope="row">{{user.id}}</th>
-      <td>{{user.displayName}}</td>
-      <td>{{user.rol}}</td>
-    </tr>
-  </tbody>
-</table>
+  <div class="container">
+    <div class="lista-usuarios">
+      <div class="card" style="width: 30rem;">
+        <h1 class="card-title">Lista de usuarios</h1>
+        <table class="table table-hover table-dark">
+          <thead class="thead-light">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Nombre:</th>
+              <th scope="col">Rol</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="(user, index) in usuarios"
+              :key="index"
+              @click="onClickUser(user)"
+            >
+              <td scope="row">{{ user.email }}</td>
+              <td>{{ user.name }}</td>
+              <td>{{ user.rol }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="side">
+      <div class="max-puntuacion">
+        <p>puntuaciones máximas</p>
+      </div>
+      <div class="ultimas-puntuaciones">
+        <p>últimas puntuaciones</p>
+
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
+import router from "../router/index";
 export default {
-  name: 'UserList',
-    async created() {
-      try {
-        await this.$store.dispatch('getUsers');
-      } catch (error) {
-        console.error(error);
-      }  
+  name: "UserList",
+  async created() {
+    try {
+      await this.$store.dispatch("getUsers");
+    } catch (error) {
+      console.error(error);
+    }
+    
+    try {
+      await this.$store.dispatch("getPuntos");
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  computed: {
+    usuarios() {
+      return this.$store.state.usuarios;
     },
-    computed: {
-      usuarios(){
-        return this.$store.state.usuarios
-      }
+    puntos(){
+      return this.$store.state.usersPoints;
+    }
+  },
+  methods: {
+    onClickUser(user) {
+      router.push({ path: `/usuario/${user.email}` });
     },
+  },
 };
 </script>
 
 <style scoped>
-.container{
-  max-width: 40em;
+.container {
+  color: black;
+  display: grid;
+  grid-template-columns: 50% 50%;
+}
+
+.side{
+  color: aliceblue;
+  border: solid red;
+}
+
+.side>div{
+  border: solid red;
+
 }
 </style>
